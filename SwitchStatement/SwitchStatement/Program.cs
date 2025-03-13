8c00175852d95a91d852424cd7d6f366bf4ec4e7
CodeWithNirmalya <itsavenger02@gmail.com>
2025-03-13 22:23:51 +0530

    Delete SwitchStatement directory

--- SwitchStatement/SwitchStatement/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more 
-Console.Write("What's your name: ");
-String name =Console.ReadLine();
-Console.WriteLine("Welcome to our community " + name);
-
-Console.Write("What's your age: ");
-int Age =  Convert.ToInt32( Console.ReadLine());
-Console.WriteLine("Your age is : "+Age);
